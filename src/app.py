a9906f9144007af27bacbed7987a027fb4480fb4
jonathanmenezesm <jonathanmenezesm@gmail.com>
2025-04-28 19:37:31 -0300

    Adiciona configuração de CORS para permitir requisições de diferentes origens

--- src/app.py
+++ src/app.py
@@ -4,9 +4,15 @@ from flask import Flask
 from src.controller.colaborador_controller import bp_colaborador
 from src.model import db
 from config import Config
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, origins="*") # <----- A politica de CORS é definida aqui, permitindo que o frontend acesse a API 
+    #Habilita o CORS para a aplicação, permitindo requisições de diferentes origens
+    #CORS Sempre tem que vir depois da instanciação do Flask e antes de registrar o blueprint
+    
+    
     app.register_blueprint(bp_colaborador)
     
     app.config.from_object(Config) #Traz a configuração do banco de dados para a aplicação
