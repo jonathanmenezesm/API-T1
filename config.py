0645b4e2ee970be78fa4a2db351fd11b8a59eac7
jonathanmenezesm <jonathanmenezesm@gmail.com>
2025-05-16 03:44:32 -0300

    Atualiza configuração do banco de dados para usar a URL de desenvolvimento e comenta a URL de produção

--- config.py
+++ config.py
@@ -5,5 +5,7 @@ load_dotenv() #Carrega as variáveis de ambiente para este arquivo
 
 class Config():
     # Configuração do banco de dados
-    SQLALCHEMY_DATABASE_URI = environ.get('URL_DATABASE_PROD') #Acessa a variável de ambiente DATABASE_URL (PRODUÇÃO)
+    # SQLALCHEMY_DATABASE_URI = environ.get('URL_DATABASE_PROD') #Acessa a variável de ambiente DATABASE_URL (PRODUÇÃO)
+    SQLALCHEMY_DATABASE_URI = environ.get('URL_DATABASE_DEV')
+    #Acessa a variável de ambiente DATABASE_URL (DESENVOLVIMENTO)
     SQLALCHEMY_TRACK_MODIFICATIONS = False #Desabilita o rastreamento de modificações do SQLAlchemy para economizar recursos.
